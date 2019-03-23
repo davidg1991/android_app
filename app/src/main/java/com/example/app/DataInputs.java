e9fff549287129260f9f8794ee8dc260313d3ba2
davidg1991 <47544110+davidg1991@users.noreply.github.com>
2019-03-23 16:32:32 -0400

    Update DataInputs.java
    Sorry, this is the right additions I've made to the current file.

--- app/src/main/java/com/example/app/DataInputs.java
+++ app/src/main/java/com/example/app/DataInputs.java
@@ -9,76 +9,153 @@ public class DataInputs
     private float engineRPM;
     private int gear;
 
-    public DataInputs(float oilTemperature, float oilPressure, float fuelPressure, float coolantTemperature, float engineRPM, int gear)
+    public DataInputs(float oilTemperature,float oilPressure,float fuelPressure, float fuelTemperature, float coolantTemperature,float engineRPM,float roomTemperature,int gear)
     {
-        this.oilPressure=oilPressure;
         this.oilTemperature=oilTemperature;
+        this.oilPressure=oilPressure;
         this.fuelPressure=fuelPressure;
+        this.fuelTemperature=fuelTemperature;
         this.coolantTemperature=coolantTemperature;
         this.engineRPM=engineRPM;
+        this.roomTemperature=roomTemperature;
         this.gear=gear;
 
     }
 
-    public float getOilTemperature()
-    {
+     public float getOilTemperature() 
+     {
         return oilTemperature;
     }
 
-    public void setOilTemperature(float oilTemperature)
-    {
+    public void setOilTemperature(float oilTemperature) {
         this.oilTemperature = oilTemperature;
     }
 
-    public float getOilPressure()
-    {
+    public float getOilPressure() {
         return oilPressure;
     }
 
-    public void setOilPressure(float oilPressure)
-    {
+    public void setOilPressure(float oilPressure) {
         this.oilPressure = oilPressure;
     }
 
-    public float getFuelPressure()
-    {
+    public float getFuelPressure() {
         return fuelPressure;
     }
 
-    public void setFuelPressure(float fuelPressure)
-    {
+    public void setFuelPressure(float fuelPressure) {
         this.fuelPressure = fuelPressure;
     }
 
-    public float getCoolantTemperature()
-    {
+    public float getFuelTemperature() {
+        return fuelTemperature;
+    }
+
+    public void setFuelTemperature(float fuelTemperature) {
+        this.fuelTemperature = fuelTemperature;
+    }
+
+    public float getCoolantTemperature() {
         return coolantTemperature;
     }
 
-    public void setCoolantTemperature(float coolantTemperature)
-    {
+    public void setCoolantTemperature(float coolantTemperature) {
         this.coolantTemperature = coolantTemperature;
     }
 
-    public float getEngineRPM()
-    {
+    public float getRoomTemperature() {
+        return roomTemperature;
+    }
+
+    public void setRoomTemperature(float roomTemperature) {
+        this.roomTemperature = roomTemperature;
+    }
+
+    public float getEngineRPM() {
         return engineRPM;
     }
 
-    public void setEngineRPM(float engineRPM)
-    {
+    public void setEngineRPM(float engineRPM) {
         this.engineRPM = engineRPM;
     }
 
-    public int getGear()
-    {
+    public int getGear() {
         return gear;
     }
 
-    public void setGear(int gear)
-    {
+    public void setGear(int gear) {
         this.gear = gear;
     }
 
+    public boolean rangeOilTemperature() {
+        if (oilTemperature >= 75.0f & oilTemperature <= 120.0f) {
+            System.out.println("The value is within the range!");
+            return true;
+        } else {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+    }
+
+    public boolean rangeOilPressure() {
+        if (oilPressure >= 400.0f & oilPressure <= 500.0f) {
+            System.out.println("The value is within the range!");
+            return true;
+        } else {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+
+    }
+
+    public boolean rangeFuelTemperature() {
+        if (fuelTemperature > roomTemperature) {
+            System.out.println("The value is within the range!");
+            return true;
+        } else {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+
+    }
+
+    public boolean rangeFuelPressure() {
+        if (fuelPressure >= 380.0f & fuelPressure <= 420.0f) {
+            System.out.println("The value is within the range!");
+            return true;
+        } else {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+
+    }
+
+    public boolean rangeCoolantTemperature() {
+        if (coolantTemperature >= 80.0f & coolantTemperature <= 100.0f) {
+            System.out.println("The value is within the range!");
+            return true;
+        } else {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+    }
+
+
+    public boolean rangeEngineRPM()
+    {
+        if (engineRPM>=12500.0f)
+        {
+            System.out.println("The value is within the range!");
+            return true;
+        }
+
+
+        else
+        {
+            System.out.println("The value is outside the range!");
+            return false;
+        }
+
+    }
 
 }
